7f3a35b2055eb06ce6570fb524f5315e0debe2b5
Monika Alluru <sree.alluru@cloudreach.com>
2022-07-20 18:29:26 +0530

    added file

--- role.tf
+++ role.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "github_assume_rolepolicy" {
           values = ["sts.awsamazon.com"]
         }
         condition {
-          test = "StringEquals"
+          test = "StringLike"
           variable = "token.actions.githubusercontent.com:sub"
           values = ["repo:SreeAlluru/*:*"]
         }
